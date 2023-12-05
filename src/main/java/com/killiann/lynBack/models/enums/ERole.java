fb00fb6fe18814ce3065a1ea8382c69bdbe77952
KilliannH <killiannhervagault@gmail.com>
2023-12-05 18:47:53 +0100

    first commit

---
+++ src/main/java/com/killiann/lynBack/models/enums/ERole.java
@@ -0,0 +1,7 @@
+package com.killiann.lynBack.models.enums;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/killiann/lynBack/ProfileCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killiann.lynBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
